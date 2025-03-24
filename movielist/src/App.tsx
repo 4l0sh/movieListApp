4502f3b120e064ebae4edf10189f978e6c7e6ae7
ALI <2155399@roc-dev.com>
2025-03-24 14:55:57 +0100

    minor changes

--- movielist/src/App.tsx
+++ movielist/src/App.tsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/homePage';
 import Signup from './pages/account/signup';
+import Login from './pages/account/login';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </Router>
     </Fragment>
--- movielist/src/pages/account/login.tsx
+++ movielist/src/pages/account/login.tsx
@@ -1,19 +1,27 @@
-import { Fragment } from 'react';
 import Navbar from '../../components/navbar';
 import './login.css';
-
-const Login = () => {
+const Signup = () => {
   return (
-    <Fragment>
-      <div className='mainContainer'>
-        <div className='signupContainer'>
-          <div className='loginCard'></div>
-          <div className='signupCard'></div>
+    <div className='mainContainer'>
+      <div className='signupContainer'>
+        <div className='signupCard'>
+          <h1>Sign In</h1>
+          <form className='signupForm'>
+            {' '}
+            <input type='email' id='email' placeholder='Email Adress' />
+            <input type='password' id='password' placeholder='Password' />
+            <input type='submit' value='Sign In ' />
+          </form>
+
+          <hr />
+          <p className='signup'>
+            Don't have an account ? <a href='login'>Sign Up</a>
+          </p>
         </div>
-        <Navbar />
       </div>
-    </Fragment>
+      <Navbar />
+    </div>
   );
 };
 
-export default Login;
+export default Signup;
